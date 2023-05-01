2c2fbb22d89c8169c8055249c5b4e227de0c0ba2
Major <107882188+qubitflip@users.noreply.github.com>
2023-05-01 16:57:08 -0500

    Progress?

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    hello_sequence();
+    timing_handler([
+        {func: hello_sequence, timing: 700}
+    ]);
 });
+
+function timing_handler(arr) {
+    arr.forEach(process => {
+        setTimeout(function () {
+            (process.func)()
+        }, process.timing)
+    })
+}
 
 function hello_sequence() {
     helloSection = document.querySelector(".hello-intro");
@@ -12,16 +22,19 @@ function hello_sequence() {
     setTimeout(function () {
         progressBar = document.querySelector(".progress-bar .bar");
         progressText = document.querySelector(".progress-bar .bar .text");
+
+        progressBar.classList.add("pb-active");
+
         for(let i = 0; i <= 100; i += 1) {
             if (i > 5) {
                 progressText.style.visibility = "visible";
                 progressText.style.opacity = "100%";
             }
             setTimeout(function () {
-                progressBar.style.width = `${i}%`;
                 progressText.innerText = `${i}%`;
             }, i * 10);
         }
+
         setTimeout(function () {
             progressText.style.transition = "opacity 0.2s, transform 0.3s";
             progressText.style.transform = "translateY(0px)";
